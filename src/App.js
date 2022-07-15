808a1d08cd41e199d8ed2b0dd33663102d050c0d
Ivan <offlane@tut.by>
2022-07-15 20:55:01 +0300

    created card and list components, using REST API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Card } from "./components/Card";
 import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
+import { List } from "./components/List";
 import { Main } from "./components/Main";
+import { ALL_COUNTRIES } from "./config";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Main>
-        <Controls />
-      </Main>
-    </>
-  );
+	const [countries, setCountries] = useState([])
+	console.log(countries)
+
+	// async await????
+	useEffect(() => {
+
+    axios.get(ALL_COUNTRIES).then(
+		({data}) => setCountries(data)
+    )
+	},[])
+
+	return (
+		<>
+			<Header />
+			<Main>
+				<Controls />
+				<List>
+					{
+						countries.map(c => {
+							const countryInfo ={
+								img: c.flags.png,
+								name: c.name.common,
+								code: c.code,
+								info: [
+									{
+										title: 'Population',
+										description: c.population.toLocaleString()
+									},
+									{
+										title: 'Region',
+										description: c.region
+									},
+									{
+										title: 'Capital',
+										description: c.capital
+									},
+								],
+							}
+
+							return (
+								<Card key={c.name.common} {...countryInfo} />
+							)
+						})
+					}
+				</List>
+			</Main>
+		</>
+	);
 }
 
 export default App;
